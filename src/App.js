330caf7e78d342be6da7d5810cf22f76df71bd7a
Bruna Terribile <brunaterribile@gmail.com>
2022-10-05 23:17:50 -0300

    React-Routes e definição das Rotas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import Header from "./components/Header"
-import Main from "./components/Main"
+import HomePage from "./pages/HomePage"
+import SessionPage from "./pages/SessionPage"
+import SeatsPage from "./pages/SeatsPage"
+import ConfirmPage from "./pages/ConfirmPage"
 import Footer from "./components/Footer"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
+      
       <Header></Header>
-      <Main></Main>
+      
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/sessoes/:idFilme" element={<SessionPage/>}/>
+        <Route path="/assentos/:idSessao" element={<SeatsPage/>}/>
+        <Route path="/sucesso" element={<ConfirmPage/>}/>
+      </Routes>
+
       <Footer></Footer>
-    </>
+    </BrowserRouter>
   );
 }
 
